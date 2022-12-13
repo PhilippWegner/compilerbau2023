852c474b2ccde57731cbbd70cd296c928aeefbab
Dominik <Dominik.elis@hs-bochum.de>
2022-12-13 14:02:53 +0100

    Added Splitter

--- Compiler/src/Main.java
+++ Compiler/src/Main.java
@@ -2,16 +2,22 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        TokenScanner scanner = new TokenScanner("C:\\Users\\Dominik\\IdeaProjects\\compilerbau2023\\Compiler\\resources\\input.txt");
+        TokenScanner scanner = new TokenScanner("resources/input.txt");
         scanner.scan();
         System.out.println("Input: "+ scanner.input+"\n");
         ArrayList<Token> tokens = scanner.tokenize();
-        System.out.println("Tokens: \n");
 
+        System.out.println("Tokens: \n");
         for(Token t : tokens)
         {
             System.out.println(t);
         }
 
+        System.out.println("Tokens: \n");
+        ArrayList<Character> chars =scanner.split();
+        for(Character c : chars)
+        {
+            System.out.println(c);
+        }
     }
 }
--- Compiler/src/Token.java
+++ Compiler/src/Token.java
@@ -1,7 +1,6 @@
 public enum Token {
     Invalid,
     Letter,
-
     Number,
     Add,
     Subtract,
